e7069b1f1fe0176414fb1201b9a8306953b917c0
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-05-21 22:57:07 +0700

    Add Product Detail MVC

---
+++ src/EShop.CustomerFe/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using EShop.CustomerFe.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.CustomerFe.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            this.productService = productService;
+        }
+        public async Task<IActionResult> Detail(Guid id)
+        {
+            var product = await productService.GetProductByIdAsync(id);
+            return View(product);
+        }
+    }
+}
