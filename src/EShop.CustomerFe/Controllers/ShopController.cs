412dbba5e7a6c94dceed00d7262fba755032cbb7
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-05-21 22:59:28 +0700

    Add Shop Page MVC

---
+++ src/EShop.CustomerFe/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using EShop.CustomerFe.Services;
+using EShop.ViewModels.ShopViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.CustomerFe.Controllers
+{
+    public class ShopController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+
+        public ShopController(ILogger<HomeController> logger, IProductService productService, ICategoryService categoryService)
+        {
+            _logger = logger;
+            this.productService = productService;
+            this.categoryService = categoryService;
+        }
+        public async Task<IActionResult> Page(Guid categoryId)
+        {
+            var products = await productService.GetProductByCategoryIdAsync(categoryId);
+            var category = products.FirstOrDefault()?.CategoryName;
+            var categories = await categoryService.GetAllCategoriesAsync();
+            var shopViewModel = new ShopViewModel
+            {
+                Products = products,
+                CategoryName = category,
+                Categories = categories
+            };
+            return View(shopViewModel);
+        }
+    }
+}
---
+++ src/EShop.ViewModels/ShopViewModel/ShopViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EShop.ViewModels.CategoryViewModel;
+using EShop.ViewModels.ProductViewModel;
+
+namespace EShop.ViewModels.ShopViewModel
+{
+    public class ShopViewModel
+    {
+        public List<ProductResponse> Products { get; set; }
+        public List<CategoryResponse> Categories { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
