85e4dd08ae1e6e12c0d25fddba621665c21897f0
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-05-22 10:38:23 +0700

    Add Header Component with categories data in MVC

---
+++ src/EShop.CustomerFe/Components/HeaderViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using EShop.CustomerFe.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.CustomerFe.Components
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public HeaderViewComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await categoryService.GetAllCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
