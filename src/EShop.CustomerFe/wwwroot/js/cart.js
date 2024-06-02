ef20eb125a25436364f972e8b88f4cf555043678
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-06-02 17:52:04 +0700

    style (mvc): Separate HTML into partial views with HTML style adjustments

---
+++ src/EShop.CustomerFe/wwwroot/js/cart.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '.button-plus', function () {
+        var quantityField = $(this).siblings('.quantity-field');
+        var currentValue = parseInt(quantityField.val());
+        if (!isNaN(currentValue) && currentValue < parseInt(quantityField.attr('max'))) {
+            quantityField.val(currentValue + 1);
+        }
+    });
+    $(document).on('click', '.button-minus', function () {
+        var quantityField = $(this).siblings('.quantity-field');
+        var currentValue = parseInt(quantityField.val());
+        if (!isNaN(currentValue) && currentValue > 1) {
+            quantityField.val(currentValue - 1);
+        }
+    });
+
+    function checkAuthentication(callback) {
+        $.ajax({
+            type: "GET",
+            url: "/Auth/IsAuthenticated",
+            success: function (isAuthenticated) {
+                callback(isAuthenticated);
+            },
+            error: function () {
+                alert('Unable to check authentication status.');
+            }
+        });
+    }
+    function addToCart(productId, quantity) {
+        $.ajax({
+            type: "POST",
+            url: "/Cart/AddToCart",
+            data: {
+                productId: productId,
+                quantity: quantity
+            },
+            success: function (response) {
+                alert('Product added to cart successfully!');
+            },
+            error: function (xhr, status, error) {
+                alert('An error occurred while adding the product to cart!');
+            }
+        });
+    }
+
+    // Event handler for Add to Cart button click
+    $(document).on('click', '#addToCartButton', function () {
+        var productId = $('#productId').val();
+        var quantity = $('#quantity').val();
+        addToCart(productId, quantity);
+    });
+});
