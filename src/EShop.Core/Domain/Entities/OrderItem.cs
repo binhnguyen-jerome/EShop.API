e40e5894ae10f1e1adff66be68ded814da024c7c
Nguyen To Binh <nguyentobinh12@gmail.com>
2024-06-02 17:44:25 +0700

    fix (domain) : add property price for OrderItem

--- src/EShop.Core/Domain/Entities/OrderItem.cs
+++ src/EShop.Core/Domain/Entities/OrderItem.cs
@@ -7,5 +7,7 @@
         public Guid ProductId { get; set; }
         public virtual Product Product { get; set; } = null!;
         public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
