0fc38b46b86b57c1c9d37c143c377faa87f0a603
nguyentobinh12x5 <nguyentobinh12@gmail.com>
2024-06-06 12:41:35 +0700

    test (application): add Cart and ProductReview Service Test

---
+++ test/EShop.UnitTest/Application/CartServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using AutoFixture;
+using EShop.Application.Services.Implements;
+using EShop.Core.Domain.Entities;
+using EShop.Core.Domain.Repositories;
+using EShop.ViewModels.Dtos.Cart;
+using Moq;
+using System.Linq.Expressions;
+namespace EShop.UnitTest.Application
+{
+    public class CartServiceTests
+    {
+        private readonly CartService _cartService;
+        private readonly Mock<IGenericRepository<Cart>> _mockCartRepository;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<ICartQueries> _mockCartQueries;
+        private readonly CustomFixture _fixture;
+        public CartServiceTests()
+        {
+            _fixture = new CustomFixture();
+
+            _mockCartRepository = new Mock<IGenericRepository<Cart>>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockCartQueries = new Mock<ICartQueries>();
+            _mockUnitOfWork.Setup(u => u.GetBaseRepo<Cart>()).Returns(_mockCartRepository.Object);
+
+            _cartService = new CartService(_mockUnitOfWork.Object, _mockCartQueries.Object);
+        }
+        [Fact]
+        public async Task UpdateCartAsync_ValidCart_ReturnCart()
+        {
+            var updateCart = _fixture.Create<CartRequest>();
+            updateCart.Quantity = 5;
+
+            var cart = _fixture.Create<Cart>();
+            cart.ApplicationUserId = updateCart.ApplicationUserId;
+            cart.ProductId = updateCart.ProductId;
+
+            _mockCartQueries.Setup(repo => repo.GetCartByUserIdAndProductIdAsync(updateCart.ApplicationUserId, updateCart.ProductId))
+                .ReturnsAsync(cart);
+
+            _mockCartRepository.Setup(repo => repo.Update(It.IsAny<Cart>()));
+
+            _mockUnitOfWork.Setup(u => u.CompleteAsync())
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _cartService.UpdateCartAsync(updateCart);
+
+            // Assert
+            Assert.True(result);
+            _mockCartRepository.Verify(repo => repo.Update(It.Is<Cart>(c => c == cart && c.Quantity == updateCart.Quantity)), Times.Once);
+            _mockUnitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+        [Fact]
+        public async Task UpdateCartAsync_InvalidCart_ThrowException()
+        {
+            var updateCart = _fixture.Create<CartRequest>();
+            updateCart.Quantity = 5;
+
+            _mockCartQueries.Setup(repo => repo.GetCartByUserIdAndProductIdAsync(updateCart.ApplicationUserId, updateCart.ProductId))
+                .ReturnsAsync((Cart)null);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => _cartService.UpdateCartAsync(updateCart));
+        }
+        [Fact]
+        public async Task UpdateCartAsync_InvalidQuantity_ThrowException()
+        {
+            var updateCart = _fixture.Create<CartRequest>();
+            updateCart.Quantity = 0;
+
+            // Act and Assert
+            await Assert.ThrowsAsync<ApplicationException>(() => _cartService.UpdateCartAsync(updateCart));
+        }
+        [Fact]
+        public async Task RemoveFromCartAsync_ValidCart_ReturnTrue()
+        {
+            var cart = _fixture.Create<Cart>();
+
+            _mockCartRepository.Setup(repo => repo.Get(It.IsAny<Expression<Func<Cart, bool>>>(), null, false))
+                .ReturnsAsync(cart);
+
+            _mockCartRepository.Setup(repo => repo.Remove(cart));
+
+            _mockUnitOfWork.Setup(u => u.CompleteAsync())
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _cartService.RemoveFromCartAsync(cart.Id);
+
+            // Assert
+            Assert.True(result);
+            _mockCartRepository.Verify(repo => repo.Remove(It.Is<Cart>(c => c == cart)), Times.Once);
+            _mockUnitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+        [Fact]
+        public async Task RemoveFromCartAsync_InvalidCart_ThrowException()
+        {
+            var cart = _fixture.Create<Cart>();
+
+            _mockCartRepository.Setup(repo => repo.Get(It.IsAny<Expression<Func<Cart, bool>>>(), null, false))
+                .ReturnsAsync((Cart)null);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => _cartService.RemoveFromCartAsync(cart.Id));
+        }
+        [Fact]
+        public async Task AddToCartAsync_InvalidQuantity_ThrowException()
+        {
+            var cartRequest = _fixture.Create<CartRequest>();
+            cartRequest.Quantity = 0;
+
+            // Act and Assert
+            await Assert.ThrowsAsync<ApplicationException>(() => _cartService.AddToCartAsync(cartRequest));
+        }
+    }
+}
---
+++ test/EShop.UnitTest/Application/ProductReviewServiceTests.cs
@@ -0,0 +1,133 @@
+﻿using AutoFixture;
+using EShop.Core.Domain.Entities;
+using EShop.Core.Domain.Repositories;
+using EShop.Core.Mappers;
+using EShop.Core.Services.Implements;
+using EShop.ViewModels.Dtos.Review;
+using Moq;
+using System.Linq.Expressions;
+namespace EShop.UnitTest.Application
+{
+    public class ProductReviewServiceTests
+    {
+        private readonly ProductReviewService _productReviewService;
+        private readonly Mock<IGenericRepository<ProductReview>> _mockProductReviewRepository;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<IProductReviewQueries> _mockProductReviewQueries;
+        private readonly CustomFixture _fixture;
+        public ProductReviewServiceTests()
+        {
+            _fixture = new CustomFixture();
+            _mockProductReviewQueries = new Mock<IProductReviewQueries>();
+            _mockProductReviewRepository = new Mock<IGenericRepository<ProductReview>>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockUnitOfWork.Setup(u => u.GetBaseRepo<ProductReview>()).Returns(_mockProductReviewRepository.Object);
+            _productReviewService = new ProductReviewService(_mockUnitOfWork.Object, _mockProductReviewQueries.Object);
+        }
+        [Fact]
+        public async Task CreateProductAsync_ValidReview_RerurnReview()
+        {
+            //Arrange
+            var createProductReview = _fixture.Create<ProductReviewRequest>();
+            var productReview = createProductReview.ToProductReview();
+            _mockProductReviewRepository.Setup(repo => repo.Add(productReview));
+            _mockUnitOfWork.Setup(u => u.CompleteAsync());
+            // Act
+            var result = await _productReviewService.CreateProductReviewAsync(createProductReview);
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ProductReviewResponse>(result);
+            Assert.Equal(productReview.Content, result.Content);
+            Assert.Equal(productReview.Rate, result.Rate);
+            _mockProductReviewRepository.Verify(u => u.Add(It.IsAny<ProductReview>()), Times.Once);
+            _mockUnitOfWork.Verify(u => u.CompleteAsync(), Times.Once);
+        }
+        [Fact]
+        public async Task UpdateProductAsync_ValidReview_RerurnReview()
+        {
+            // Arrange
+            var productReviewRequest = _fixture.Create<UpdateProductReviewRequest>();
+            var productReview = productReviewRequest.ToProductReview();
+            _mockProductReviewRepository.Setup(repo => repo.Get(It.IsAny<Expression<Func<ProductReview, bool>>>(), null, false))
+                .ReturnsAsync(productReview);
+            _mockProductReviewRepository.Setup(repo => repo.Update(productReview));
+            _mockUnitOfWork.Setup(u => u.CompleteAsync());
+
+            // Act
+            var result = await _productReviewService.UpdateProductReviewAsync(productReview.Id, productReviewRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ProductReviewResponse>(result);
+            Assert.Equal(productReview.Content, result.Content);
+            Assert.Equal(productReview.Rate, result.Rate);
+        }
+        [Fact]
+        public async Task DeleteProductAsync_ValidReview_RerurnTrue()
+        {
+            // Arrange
+            var productReview = _fixture.Create<ProductReview>();
+            _mockProductReviewRepository.Setup(repo => repo.Get(It.IsAny<Expression<Func<ProductReview, bool>>>(), null, false))
+                .ReturnsAsync(productReview);
+            _mockProductReviewRepository.Setup(repo => repo.Remove(productReview));
+            _mockUnitOfWork.Setup(u => u.CompleteAsync());
+
+            // Act
+            var result = await _productReviewService.DeleteProductReviewAsync(productReview.Id);
+
+            // Assert
+            Assert.True(result);
+        }
+        [Fact]
+        public async Task GetProductReviewsAsync_ValidQuery_ReturnListReview()
+        {
+            // Arrange
+            var productReviewQuery = _fixture.Create<ProductReviewQuery>();
+            var productReviews = _fixture.CreateMany<ProductReview>().ToList();
+            _mockProductReviewQueries.Setup(repo => repo.GetFilteredProductReviewsAsync(productReviewQuery))
+                .ReturnsAsync(productReviews);
+
+            // Act
+            var result = await _productReviewService.GetProductReviewsAsync(productReviewQuery);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<ProductReviewResponse>>(result);
+            Assert.Equal(productReviews.Count, result.Count);
+        }
+        [Fact]
+        public async Task GetProductReviewsAsync_EmptyQuery_ReturnListReview()
+        {
+            // Arrange
+            var productReviewQuery = new ProductReviewQuery();
+            var productReviews = _fixture.CreateMany<ProductReview>().ToList();
+            _mockProductReviewQueries.Setup(repo => repo.GetFilteredProductReviewsAsync(productReviewQuery))
+                .ReturnsAsync(productReviews);
+
+            // Act
+            var result = await _productReviewService.GetProductReviewsAsync(productReviewQuery);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<ProductReviewResponse>>(result);
+            Assert.Equal(productReviews.Count, result.Count);
+        }
+        [Fact]
+        public async Task GetProductReviewsAsync_NullQuery_ReturnListReview()
+        {
+            // Arrange
+            var productReviews = _fixture.CreateMany<ProductReview>().ToList();
+            _mockProductReviewQueries.Setup(repo => repo.GetFilteredProductReviewsAsync(null))
+                .ReturnsAsync(productReviews);
+
+            // Act
+            var result = await _productReviewService.GetProductReviewsAsync(null);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<ProductReviewResponse>>(result);
+            Assert.Equal(productReviews.Count, result.Count);
+        }
+
+    }
+}
